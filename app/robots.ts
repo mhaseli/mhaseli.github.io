be5ae267020f5997c960e411a056a59de77df701
masih <mhaseli@ucsd.edu>
2026-08-07 15:52:57 -0700

    Add stable Search Console sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -12,6 +12,6 @@ export default function robots(): MetadataRoute.Robots {
                 allow: "/",
             },
         ],
-        sitemap: `${siteUrl}/sitemap.xml`,
+        sitemap: `${siteUrl}/sitemap-pages.xml`,
     };
 }
